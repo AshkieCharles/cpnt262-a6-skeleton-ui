95ef212af73bf6608ef2223740ef38058358011f
Ashkie Esperanza <ashkiecharles@gmail.com>
2023-11-15 10:11:13 -0700

    added new changes

--- src/routes/(index and about)/+page.svelte
+++ src/routes/(index and about)/+page.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <List />
-<h1 class="h-screen mt-12">
+<h1 class="h-screen mx-12 my-12">
   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro aliquam aut reiciendis vero voluptatum rerum totam laboriosam dolor quasi suscipit quisquam perferendis cum exercitationem fugiat, omnis et doloremque pariatur earum?
 </h1>
 
