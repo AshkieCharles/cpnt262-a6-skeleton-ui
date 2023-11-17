68dcd65fbfffa13656ad992916de78b0f047ef41
Ashkie <ashkiecharles@gmail.com>
2023-11-17 11:06:55 -0700

    added css

--- src/routes/(index and about)/about/+page.svelte
+++ src/routes/(index and about)/about/+page.svelte
@@ -1,6 +1,6 @@
 <script>
 </script>
-<p class="h-screen mt-12">
+<p class="h-screen mx-12 my-12">
   Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, odio iste placeat aliquid natus laboriosam et obcaecati fugit consequuntur adipisci fugiat ducimus voluptates, voluptate dolore ad mollitia? Officiis, laborum doloremque!
 
 </p>
