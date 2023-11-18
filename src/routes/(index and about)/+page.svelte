2212bf55d43e0128282a952d3037f0acf53e2436
Ashkie <ashkiecharles@gmail.com>
2023-11-17 20:32:20 -0700

    added things

--- src/routes/(index and about)/+page.svelte
+++ src/routes/(index and about)/+page.svelte
@@ -2,7 +2,7 @@
   import List from "../../lib/List.svelte";
 </script>
 
-<List />
+<List class="h-screen"/>
 <p class="h-screen mx-12 my-12">
   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro aliquam aut reiciendis vero voluptatum rerum totam laboriosam dolor quasi suscipit quisquam perferendis cum exercitationem fugiat, omnis et doloremque pariatur earum?
 </p>
