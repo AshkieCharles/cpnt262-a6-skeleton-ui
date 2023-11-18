e2784ff4ac1b73e44e4e303e9200def30d125cd8
Ashkie <ashkiecharles@gmail.com>
2023-11-17 20:51:51 -0700

    added everything

--- src/routes/(gallery)/gallery/+page.svelte
+++ src/routes/(gallery)/gallery/+page.svelte
@@ -7,8 +7,7 @@ href="https://www.pexels.com/photo/shallow-focus-photography-of-red-apple-on-gra
 	<section class="p-4"><img class="m-auto h-50 w-[60rem]" src="../images/apple.png" alt="apple"></section>
 	<footer class="card-footer text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis voluptate maiores quasi blanditiis quisquam alias consectetur, accusamus voluptates doloremque officiis enim repellendus suscipit natus, ipsum excepturi, at recusandae aspernatur debitis!</footer>
 </div>
+
  </a>
-
-
 
 
