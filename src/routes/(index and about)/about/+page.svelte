1d37366ccc6aca42448cfcacd82a2740f1755eb7
Ashkie <ashkiecharles@gmail.com>
2023-11-17 17:03:17 -0700

    added changes

--- src/routes/(index and about)/about/+page.svelte
+++ src/routes/(index and about)/about/+page.svelte
@@ -1,6 +1,6 @@
-<script>
-</script>
+
 <p class="h-screen mx-12 my-12">
   Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, odio iste placeat aliquid natus laboriosam et obcaecati fugit consequuntur adipisci fugiat ducimus voluptates, voluptate dolore ad mollitia? Officiis, laborum doloremque!
+  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore quas ex autem. Similique rerum modi ducimus magni dolores quos, suscipit, nisi possimus, quam ipsum fugit aliquid natus nam ut eveniet.
 
 </p>
